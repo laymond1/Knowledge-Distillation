47405b6cabbe7369807ad38aadedd4f211c6a434
laymond1 <costor107@gmail.com>
2020-09-30 01:01:34 +0900

    'modified'

--- distillnetwork_loss.py
+++ distillnetwork_loss.py
@@ -1,12 +1,70 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from collections import namedtuple
+
+# DistillationLossWeights = namedtuple('DistillationLossWeights',
+#                                      ['distill', 'subnet', 'origin'])
+DistillationLossWeights = namedtuple('DistillationLossWeights',
+                                     ['distill', 'subnet'])
+PolicyLoss = namedtuple('PolicyLoss', ['overall_loss', 'loss_components'])
+                     
+
+# class loss(nn.Module):
+#     def __init__(self, origin_model, subnet_model, temperature=1.0, loss_weights=DistillationLossWeights(0.5,0.5)):
+#         super(loss, self).__init__()
+#         self.origin_model = origin_model
+#         self.subnet_model = subnet_model
+#         self.temperature = temperature
+#         self.loss_weights = loss_weights
+
+#         self.last_origin_logits = None
+#         self.last_subnet_logits = None
+
+#         self.loss_distill = None
+#         self.loss_subnet = None
+
+
+#     def forward(self, input, target):
+
+
 
 
 class loss(nn.Module):
-    def __init__(self):
+    def __init__(self, origin_model, subnet_model, temperature=1.0, loss_weights=DistillationLossWeights(0.5,0.5,0)):
         super(loss, self).__init__()
-        self.origin = origin
+        self.origin_model = origin_model
+        self.subnet_model = subnet_model
+        self.temperature = temperature
+        self.loss_weights = loss_weights
 
-    def forward
+        self.last_origin_logits = None
+        self.last_subnet_logits = None
 
+        self.loss_distill = None
+        self.loss_origin = None
+        self.loss_subnet = None
+
+
+    def forward(self, input, target):
+
+        if self.loss_weights.origin == 0:
+            with torch.no_grad():
+                self.last_origin_logits = self.origin_model(input)
+        else:
+            self.last_origin_logits = self.origin_model(input)
+
+        self.last_subnet_logits = self.subnet_model(input)
+
+        soft_labels = F.log_softmax(self.last_origin_logits / self.temperature, dim=1)
+        soft_preds = F.log_softmax(self.last_subnet_logits / self.temperature, dim=1)
+        preds = F.log_softmax(self.last_subnet_logits, dim=1)
+
+        self.loss_subnet = F.kl_div(preds, target) 
+        self.loss_distill = F.kl_div(soft_preds, soft_labels)
+        loss = self.loss_subnet * self.loss_weights.subnet + self.loss_distill * self.loss_weights.distill
+        
+        return loss
+
+
+            
